ac528ab6ed026136cf2df2379ba8d3bac63b93e5
Abisola Adebiyi <abisola.ca@gmail.com>
2023-03-22 15:18:15 +0100

    Remove svg import

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import "./Filters.scss";
-import search from "../../assets/icons/search.svg";
 import { IFilters } from "../../utils/Interfaces/interfaces";
 
 const Filters: FunctionComponent<IFilters> = ({
@@ -27,7 +26,28 @@ const Filters: FunctionComponent<IFilters> = ({
     <div className="Filters">
       {/* search  */}
       <div className="Filters-SearchWrapper">
-        <img src={search} />
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+            stroke="#7E7E7E"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M21 21L16.65 16.65"
+            stroke="#7E7E7E"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
         <input
           type="text"
           placeholder="Search"
